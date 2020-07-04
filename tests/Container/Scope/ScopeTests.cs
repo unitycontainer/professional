e56132a263bc8976685a47ed97c9e915279463be
Eugene Sadovoi <evgeni@eniks.com>
2020-07-04 09:39:15 -0700

    Adding more tests

---
+++ tests/Container/Scope/ScopeTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity.Scope;
+
+namespace Container.Scope
+{
+    [TestClass]
+    public class ScopeAsyncTests : ScopeTests
+    {
+        [TestInitialize]
+        public override void InitializeTest() => Scope = new ContainerScopeAsync(Container);
+    }
+}
